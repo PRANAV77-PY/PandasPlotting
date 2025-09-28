7cb75c5d2f3e48b0b9d7d58008d9393d5fcbd3e8
PRANAV77-PY <pbse1377@gmail.com>
2025-09-28 18:30:28 +0530

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('D:\Software engg Professional training\Python Basics\Python Pandas\pandas plot\data1.csv')
+
+df.plot()
+
+plt.show()
